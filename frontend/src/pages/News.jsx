997ed04dfdb2f6b271b6ff17591cf3a5bedff355
Faizan Alam <alamf6023@gmail.com>
2024-09-11 12:38:53 +0530

    complete design of news page

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -1,5 +1,225 @@
-export default function News(){
-    return(
-        <div>News</div>
-    )
+export default function News() {
+  return (
+    <div className="bgimage">
+      <h3 className="text-center text-3xl uppercase pt-2 font-bold">EV News</h3>
+      <div className="mx-3 mt-3 flex flex-wrap gap-5 justify-center items-center flex-col p-2">
+
+
+
+
+        <div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+          <div className="flex gap-4">
+            <p className="border p-1 rounded">Political</p>
+            <p className="p-1">18 March 2024</p>
+          </div>
+          <div className="mt-2">
+            <h1 className="font-bold capitalize">
+              The EV Mechanism are the best thign i can say it exists
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <br /> Veniam optio amet magni porro tempore eius ut quo ...
+            </p>
+            <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+              Continue Reading
+            </a>
+          </div>
+        </div>
+        <div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+          <div className="flex gap-4">
+            <p className="border p-1 rounded">Political</p>
+            <p className="p-1">18 March 2024</p>
+          </div>
+          <div className="mt-2">
+            <h1 className="font-bold capitalize">
+              The EV Mechanism are the best thign i can say it exists
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <br /> Veniam optio amet magni porro tempore eius ut quo ...
+            </p>
+            <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+              Continue Reading
+            </a>
+          </div>
+        </div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+        <div className="flex gap-4">
+          <p className="border p-1 rounded">Political</p>
+          <p className="p-1">18 March 2024</p>
+        </div>
+        <div className="mt-2">
+          <h1 className="font-bold capitalize">
+            The EV Mechanism are the best thign i can say it exists
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <br /> Veniam optio amet magni porro tempore eius ut quo ...
+          </p>
+          <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+            Continue Reading
+          </a>
+        </div>
+      </div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+      <div className="flex gap-4">
+        <p className="border p-1 rounded">Political</p>
+        <p className="p-1">18 March 2024</p>
+      </div>
+      <div className="mt-2">
+        <h1 className="font-bold capitalize">
+          The EV Mechanism are the best thign i can say it exists
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <br /> Veniam optio amet magni porro tempore eius ut quo ...
+        </p>
+        <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+          Continue Reading
+        </a>
+      </div>
+    </div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+    <div className="flex gap-4">
+      <p className="border p-1 rounded">Political</p>
+      <p className="p-1">18 March 2024</p>
+    </div>
+    <div className="mt-2">
+      <h1 className="font-bold capitalize">
+        The EV Mechanism are the best thign i can say it exists
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <br /> Veniam optio amet magni porro tempore eius ut quo ...
+      </p>
+      <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+        Continue Reading
+      </a>
+    </div>
+  </div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+  <div className="flex gap-4">
+    <p className="border p-1 rounded">Political</p>
+    <p className="p-1">18 March 2024</p>
+  </div>
+  <div className="mt-2">
+    <h1 className="font-bold capitalize">
+      The EV Mechanism are the best thign i can say it exists
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <br /> Veniam optio amet magni porro tempore eius ut quo ...
+    </p>
+    <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+      Continue Reading
+    </a>
+  </div>
+</div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+<div className="flex gap-4">
+  <p className="border p-1 rounded">Political</p>
+  <p className="p-1">18 March 2024</p>
+</div>
+<div className="mt-2">
+  <h1 className="font-bold capitalize">
+    The EV Mechanism are the best thign i can say it exists
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <br /> Veniam optio amet magni porro tempore eius ut quo ...
+  </p>
+  <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+    Continue Reading
+  </a>
+</div>
+</div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+<div className="flex gap-4">
+  <p className="border p-1 rounded">Political</p>
+  <p className="p-1">18 March 2024</p>
+</div>
+<div className="mt-2">
+  <h1 className="font-bold capitalize">
+    The EV Mechanism are the best thign i can say it exists
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <br /> Veniam optio amet magni porro tempore eius ut quo ...
+  </p>
+  <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+    Continue Reading
+  </a>
+</div>
+</div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+<div className="flex gap-4">
+  <p className="border p-1 rounded">Political</p>
+  <p className="p-1">18 March 2024</p>
+</div>
+<div className="mt-2">
+  <h1 className="font-bold capitalize">
+    The EV Mechanism are the best thign i can say it exists
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <br /> Veniam optio amet magni porro tempore eius ut quo ...
+  </p>
+  <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+    Continue Reading
+  </a>
+</div>
+</div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+<div className="flex gap-4">
+  <p className="border p-1 rounded">Political</p>
+  <p className="p-1">18 March 2024</p>
+</div>
+<div className="mt-2">
+  <h1 className="font-bold capitalize">
+    The EV Mechanism are the best thign i can say it exists
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <br /> Veniam optio amet magni porro tempore eius ut quo ...
+  </p>
+  <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+    Continue Reading
+  </a>
+</div>
+</div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+<div className="flex gap-4">
+  <p className="border p-1 rounded">Political</p>
+  <p className="p-1">18 March 2024</p>
+</div>
+<div className="mt-2">
+  <h1 className="font-bold capitalize">
+    The EV Mechanism are the best thign i can say it exists
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <br /> Veniam optio amet magni porro tempore eius ut quo ...
+  </p>
+  <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+    Continue Reading
+  </a>
+</div>
+</div><div className="border p-5 shadow-sm shadow-black rounded hover:shadow-md hover:shadow-black w-full md:w-1/2 bg-white">
+<div className="flex gap-4">
+  <p className="border p-1 rounded">Political</p>
+  <p className="p-1">18 March 2024</p>
+</div>
+<div className="mt-2">
+  <h1 className="font-bold capitalize">
+    The EV Mechanism are the best thign i can say it exists
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <br /> Veniam optio amet magni porro tempore eius ut quo ...
+  </p>
+  <a href="#" className="py-2 rounded text-blue-500 font-bold text-sm underline">
+    Continue Reading
+  </a>
+</div>
+</div>
+
+
+
+
+
+
+      </div>
+    </div>
+  );
 }
