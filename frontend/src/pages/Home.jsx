a549729d198c9805482e49c9ccf4623c29bf8057
Faizan Alam <alamf6023@gmail.com>
2024-09-14 15:56:14 +0530

    minor update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,19 +11,19 @@ export default function Home(){
 	</button>
 	<div className="flex items-center justify-start w-full h-full gap-6 py-4 mx-auto overflow-auto lg:gap-8">
 		<div className="relative flex flex-shrink-0 w-full sm:w-auto">
-			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/?1" alt="Image 1" />
+			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/1" alt="Image 1" />
 		</div>
 		<div className="relative flex flex-shrink-0 w-full sm:w-auto">
-			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/?2" alt="Image 2" />
+			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/2" alt="Image 2" />
 		</div>
 		<div className="relative flex flex-shrink-0 w-full sm:w-auto">
-			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/?3" alt="Image 3" />
+			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/3" alt="Image 3" />
 		</div>
 		<div className="relative flex flex-shrink-0 w-full sm:w-auto">
-			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/?4" alt="Image 4" />
+			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/4" alt="Image 4" />
 		</div>
 		<div className="relative flex flex-shrink-0 w-full sm:w-auto">
-			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/?5" alt="Image 5" />
+			<img className="object-cover object-center dark:bg-gray-500 h-96 aspect-square" src="https://source.unsplash.com/random/240x360/5" alt="Image 5" />
 		</div>
 	</div>
 	<button aria-label="Slide forward" id="next" className="absolute right-0 z-30 p-2 mr-10 bg-opacity-50 rounded-full focus:outline-none focus:dark:bg-gray-600 focus:ring-2 focus:ring-offset-2 focus:dark:ring-gray-600 dark:bg-gray-50">
